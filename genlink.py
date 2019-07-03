6d7eec9352e8f7450ad6de2efab2234ef49d8fd5
Jinghui Hu <hujinghui@buaa.edu.cn>
2019-07-03 22:55:36 +0800

    整理文件，安装创建时间排序

--- genlink.py
+++ genlink.py
@@ -2,16 +2,18 @@
 # -*- coding: utf-8 -*-
 import os
 import sys
+import time
 
 
-def getmeta(fullpath):
-  info = {'fullpath': fullpath}
-  with open(fullpath, 'r', encoding='utf-8') as f:
+def parsemeta(url):
+  info = {'url': url}
+  with open(url, 'r', encoding='utf-8') as f:
     for line in f:
       if line.startswith('#+TITLE:'):
         info['title'] = line.replace('#+TITLE:', '').strip()
       if line.startswith('#+DATE:'):
-        info['date'] = line.replace('#+DATE:', '').strip()
+        str = line.replace('#+DATE:', '').strip()
+        info['date'] = time.strptime(str, '<%Y-%m-%d %a>')
     return info
 
 
@@ -20,23 +22,21 @@ def getinfos(dir):
   for root, dirs, files in os.walk(dir):
     for file in files:
       if file.endswith('.org'):
-        fullpath = os.path.join(root, file)
-        infos.append(getmeta(fullpath))
+        url = os.path.join(root, file)
+        infos.append(parsemeta(url))
     return infos
 
 
 def infos2text(infos):
+  infos.sort(key=lambda x: -time.mktime(x['date']))
   titles = []
-  i = 1
-  for e in infos:
-    line = '{no:02d}. [[./{path}][{title}]] {date}'.format(
-        no=i,
-        path=e['fullpath'].replace('\\', '/'),
-        title=e['title'],
-        date=e['date']
-    )
-    i += 1
-    titles.append(line)
+  for index, info in enumerate(infos, start=1):
+    titles.append('{index:02d}. [[./{path}][{title}]] {date}'.format(
+        index=index,
+        path=info['url'].replace('\\', '/'),
+        title=info['title'],
+        date=time.strftime('%Y-%m-%d', info['date'])
+    ))
   return '\n'.join(titles)
 
 
