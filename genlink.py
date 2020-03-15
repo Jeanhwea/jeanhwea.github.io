6e58bd1df979eb02c6299fda914f4afb92a2d6b4
Jinghui Hu <hujinghui@buaa.edu.cn>
2020-03-15 21:06:05 +0800

    refactoring 01

--- genlink.py
+++ genlink.py
@@ -50,6 +50,8 @@ if __name__ == '__main__':
   # content = '* 目录'
   content = readdir('技术文章', 'article', by='date')
   content += readdir('编程语言', 'lang', by='name')
+  content += readdir('前端技术', 'frontend', by='name')
+  content += readdir('后端技术', 'backend', by='name')
   content += readdir('开发工具', 'tool', by='name')
   content += readdir('数据库', 'database', by='name')
   content += readdir('杂文随笔', 'misc', by='date')
